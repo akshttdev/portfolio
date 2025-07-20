89cca1653f0a69e8369e95294f1eeac7f7810869
Akshat <akshttt.dev@gmail.com>
2025-07-20 07:42:06 +0530

    Navbar Completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,31 @@
 "use client";
 import Link from "next/link";
 import TextScramble from "./TextScramble";
+import { animate } from "framer-motion";
+
 
 export const Navbar = () => {
-  // Uses the current year dynamically.
   const currentYear = new Date().getFullYear();
 
   return (
     <nav className="fixed top-0 w-full z-50 px-6 py-6 select-none mix-blend-exclusion">
       <ul className="relative w-full h-full">
-        {/* Left Item: Akshat */}
         <li className="absolute left-0 top-1/2 -translate-y-1/2">
-          <Link href="/" className="nav-txt">
+          <span
+            onClick={() =>
+                animate(window.scrollY, 0, {
+                duration: 1,
+                onUpdate: (latest) => window.scrollTo(0, latest),
+                })
+            }
+            className="nav-txt cursor-pointer">
             <TextScramble enterText="AKSHAT" />
-          </Link>
+        </span>
         </li>
 
-        {/* Center Group: About & Year */}
         <li className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex items-center gap-x-36">
             <div className="w-[5rem] text-left">
-              {/* FIX: Moved nav-txt class to the Link tag */}
               <Link href="/about" className="nav-txt">
                 <TextScramble enterText="ABOUT" />
               </Link>
@@ -29,7 +34,6 @@ export const Navbar = () => {
           </div>
         </li>
 
-        {/* Right Group: Social Links */}
         <li className="absolute right-0 top-1/2 -translate-y-1/2">
           <div className="flex items-center gap-x-6">
             <a
@@ -49,7 +53,6 @@ export const Navbar = () => {
               LN
             </a>
             <div className="w-[4rem] text-left">
-              {/* FIX: Moved nav-txt class to the <a> tag */}
               <a href="mailto:akshttt.dev@gmail.com"  className="nav-txt">
                 <TextScramble enterText="MAIL" />
               </a>
